0b7c27e6fa679db7df79f9749f4ad9797a0ae18f
nijamitsu <9869930+nijamitsu@users.noreply.github.com>
2025-02-24 17:23:55 +0300

    2

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,24 +1,22 @@
 export async function GET() {
     const baseUrl = 'https://maprat.com';
     const routes = [
-        '/',
-        '/passport',
-        '/about',
+        { path: '/', priority: 1.0 },
+        { path: '/passport', priority: 0.9 },
+        { path: '/about', priority: 0.7 },
     ];
     const lastmod = new Date().toISOString().split('T')[0];
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${routes
-        .map(route => `
-    <url>
-        <loc>${baseUrl}${route}</loc>
-        <lastmod>${lastmod}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>`)
-        .join('')}
-</urlset>`;
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${routes.map(({ path, priority }) => `
+        <url>
+            <loc>${baseUrl}${path}</loc>
+            <lastmod>${lastmod}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>${priority}</priority>
+        </url>`).join('')}
+    </urlset>`;
 
     return new Response(xml, {
         headers: { 'Content-Type': 'application/xml' }
